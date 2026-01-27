16d557ae639bb7f8ec909b1e95ca2811a796186b
AMRENDRA SINGH <142249591+techaks@users.noreply.github.com>
2026-01-27 15:37:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (71.28%) - LeetSync

---
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int ans = 0;
+        
+        while (head != nullptr) {
+            ans = (ans << 1) | head->val;
+            head = head->next;
+        }
+        
+        return ans;
+    }
+};
